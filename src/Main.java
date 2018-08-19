9bc22e117e0b0eaa5a46b77dc7fcd07cdd869622
Michał Litwa <michlithr@gmail.com>
2018-08-19 13:52:28 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import api.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User("Paweł","Cwik","32131212");
+        LibraryAPIv2 libraryAPIv2 = new LibraryAPIv2Impl();
+        APIAdapter apiAdapter = new APIAdapter(libraryAPIv2, user);
+        BookConnector connector = new BookConnector(user,apiAdapter);
+        System.out.println(connector.checkAviability("Harry Potter i Zakon Feniksa"));
+
+    }
+}
---
+++ src/api/LibraryAPI.java
@@ -0,0 +1,9 @@
+package api;
+import java.time.LocalDate;
+
+public interface LibraryAPI {
+    boolean isAviable(String bookTitle);
+    LocalDate dueDate(String bookTitle, String pesel);
+    boolean reserve(String bookTitle, String pesel);
+
+}
